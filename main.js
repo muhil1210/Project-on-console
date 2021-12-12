76bc052a90accfd337ccea3a13282093d6e8b3ff
muhil1210 <94497374+muhil1210@users.noreply.github.com>
2021-12-11 20:14:01 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("print");
+
+var first_name = "Ranbir";
+
+var last_name = "Kapoor";
+
+
+var fl = first_name.concat(last_name);
+console.log(fl);
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+console.log(add);
